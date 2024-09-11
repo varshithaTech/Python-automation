fa3e7192fa54f057598882e9a53c8ff7c9031cca
VIJASNI <varshi141178@gmail>
2024-09-11 19:32:21 +0530

    Initial Files

---
+++ timer.py
@@ -0,0 +1,45 @@
+
+
+# This is a simple bot with schedule timer
+# https://schedule.readthedocs.io
+
+import time, threading, schedule
+from telebot import TeleBot
+
+API_TOKEN = '<6803474547:AAHrUaQUOiGC65FuG8aRN6guDUm98ohI6M0>'
+bot = TeleBot(API_TOKEN)
+
+
+@bot.message_handler(commands=['help','start'])
+def send_welcome(message):
+    bot.reply_to(message,"Hi! Use /set <seconds> to set a timer")
+
+
+def beep(chat_id) -> None:
+    """Send the beep message."""
+    chat_id=5865886181
+    bot.send_message(chat_id, text='Beep!')
+
+
+@bot.message_handler(commands=['set'])
+def set_timer(message):
+    args = message.text.split()
+    if len(args) > 1 and args[1].isdigit():
+        sec = int(args[1])
+        schedule.every(sec).seconds.do(beep, message.chat.id).tag(message.chat.id)
+    else:
+        bot.reply_to(message,'Usage: /set <seconds>')
+
+
+@bot.message_handler(commands=['unset'])
+def unset_timer(message):
+    schedule.clear(message.chat.id)
+
+
+if __name__ == '__main__':
+    threading.Thread(target=bot.infinity_polling,kwargs={'poll_interval': 5}, name='bot_infinity_polling', daemon=True).start()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
